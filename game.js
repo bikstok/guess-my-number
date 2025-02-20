1653d447906db2611bdca6b3b22e6a496e0bdced
bikstok <jkjaerulff@gmail.com>
2025-02-20 11:58:15 +0100

    added streak and highscore

--- game.js
+++ game.js
@@ -3,24 +3,48 @@ const inputField = document.querySelector("#userGuess");
 
 const guessButton = document.querySelector("#guessButton");
 
-const result = guessButton.querySelectorAll("#result");
+const reloadButton = document.querySelector("#reloadButton")
+
+const currentStreakDisplay = document.querySelector("#streak");
+
+const highScoreStreakDisplay = document.querySelector("#highScoreStreak");
 //Add an eventListener that takes input from the input field
 //Print something to the console
+
+let currentStreak = 0
+let highScoreStreak = 0
+
 guessButton.addEventListener("click", (e) => {
     let guess = parseInt(inputField.value);
+
     console.log(guess);
     if (guess === randomNumber) {
         console.log("congratz");
-        document.querySelector("#result").innerText = "congratz";
+        document.querySelector("#result").innerText = "Congratz you guessed the right number";
+        document.querySelector("#higherOrLower").innerText = "";
 
+        currentStreak++
+        if (currentStreak > highScoreStreak) {
+            highScoreStreak = currentStreak
+        }
+
+        highScoreStreakDisplay.innerText = `${highScoreStreak}`;
+        currentStreakDisplay.innerText = `${currentStreak}`;
+        randomNumber = generateNumber();
+        console.log("New random number:", randomNumber);
     } else {
         console.log("you failed");
-        document.querySelector("#result").innerText = "failed";
+        document.querySelector("#result").innerText = "You guessed wrong";
         higherOrLower(guess, randomNumber);
+        currentStreak = 0;
+        currentStreakDisplay.innerText = `${currentStreak}`;
+
     }
 })
 
-
+reloadButton.addEventListener("click", () => {
+    location.reload()
+})
 
 //Make a random number between 1-5
     let randomNumber = generateNumber();
@@ -39,3 +63,4 @@ function higherOrLower(guess, randomNumber) {
         document.querySelector("#higherOrLower").innerText = "Guess higher";
     }
 }
+
